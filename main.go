2ddd7fcad8b51c24a31a8f1afa236a0152f9947f
afonso.araujo <afonso.araujo@supertroco.com.br>
2024-08-10 22:33:54 -0300

    adição do chi router + middleware

--- main.go
+++ main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	// criando um novo roteador
+	router := chi.NewRouter()
+	// configurando o middleware p/ logar as requisições
+	router.Use(middleware.Logger)
+
+	// criando uma rota simples
+	router.Get("/ola", basicHandler)
+
+	// criando um servidor
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
+	// error handling
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Falha crítica!", err)
@@ -17,5 +30,5 @@ func main() {
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Daleeeee"))
+	w.Write([]byte("Olá!"))
 }
