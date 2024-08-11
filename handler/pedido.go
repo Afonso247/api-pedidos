4d4756ae597ac6d2b4975f8c8dfd3dfb62f64a0f
afonso.araujo <afonso.araujo@supertroco.com.br>
2024-08-11 10:30:47 -0300

    Inserção da pasta handler

---
+++ handler/pedido.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Pedido struct{}
+
+func (p *Pedido) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Pedido criado com sucesso!")
+}
+
+func (p *Pedido) List(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Listando todos os pedidos...")
+}
+
+func (p *Pedido) GetByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Pegando um pedido pelo ID...")
+}
+
+func (p *Pedido) UpdateByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Atualizando um pedido pelo ID...")
+}
+
+func (p *Pedido) DeleteByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Deletando um pedido pelo ID...")
+}
--- main.go
+++ main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/Afonso247/api-pedidos/app"
 )
 
 func main() {
-	// criando um novo roteador
-	router := chi.NewRouter()
-	// configurando o middleware p/ logar as requisições
-	router.Use(middleware.Logger)
+	app := app.New()
 
-	// criando uma rota simples
-	router.Get("/ola", basicHandler)
-
-	// criando um servidor
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	// error handling
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Falha crítica!", err)
+		fmt.Println("falha em iniciar a aplicação:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Olá!"))
 }
