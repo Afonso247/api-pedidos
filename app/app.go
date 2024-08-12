9f1660896af68af7950ba0d2ab0e554daea1e62c
Afonso247 <afonso.h2002@gmail.com>
2024-08-12 12:36:30 -0300

    Inserção do Redis

--- app/app.go
+++ app/app.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 // cria uma nova instância de App com um roteador carregado
 func New() *App {
 	app := &App{
 		router: loadRouter(), // loadRouter, from routes.go
+		rdb: redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -27,7 +31,14 @@ func (a *App) Start(ctx context.Context) error {
 	}
 
 	// err handling
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("falha na conexão com o redis: %w", err)
+	}
+
+	fmt.Println("Iniciando o servidor...")
+
+	err = server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("falha ao iniciar o servidor: %w", err)
 	}
