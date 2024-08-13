7119b223258be02cdf59955a91359cd28965a173
Afonso247 <afonso.h2002@gmail.com>
2024-08-13 09:31:23 -0300

    criando o modelo 'pedido'

---
+++ model/pedido.go
@@ -0,0 +1,19 @@
+package model
+
+import "github.com/google/uuid"
+
+type Pedido struct{
+
+	PedidoID uint64 `json:"pedido_id"`
+	ClienteID uuid.UUID `json:"cliente_id"`
+	LineItems []LineItem `json:"line_items"`
+	CriadoEm *time.Time `json:"criado_em"`
+	EnviadoEm *time.Time `json:"enviado_em"`
+	ConcluidoEm *time.Time `json:"concluido_em"`
+}
+
+type LineItem struct{
+	ItemID uuid.UUID `json:"item_id"`
+	Quantidade uint `json:"quantidade"`
+	Preco uint `json:"preco"`
+}
